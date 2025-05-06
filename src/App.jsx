d56c8e4cd68130dbcd43079737bd8b03d4431396
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-05 20:43:16 -0500

    Avance rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import './styles/App.css'
-
+import {Routes} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 function App() {
   
   return (
     
-    <div>
-    <Navbar>
-    </Navbar>
-    <Login/>
+    <div className='App'>
+    <Navbar/>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  
   <React.StrictMode>
+    <BrowserRouter>
       <App />
-  </React.StrictMode>,
-)
+    </BrowserRouter>
+  </React.StrictMode>
+);
