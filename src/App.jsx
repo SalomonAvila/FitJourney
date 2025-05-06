bfaf5c6c7c3ec4d407a4a2231ba7052406d1fd19
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-05 21:19:29 -0500

    Correccion

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className='App'>
     <Navbar/>
       <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='*' element={<NoEncontrado/>}/>
       </Routes>
