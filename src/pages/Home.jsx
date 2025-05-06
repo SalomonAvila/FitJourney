a84238ccf0a8dc9b86457213643322be5b91e86e
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-05 21:13:27 -0500

    Avance estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import '../styles/Home.css'
 function Home(){
     return (
-        <div>Home</div>
+        <div id="titulo">Home</div>
     )
 }
 export default Home
