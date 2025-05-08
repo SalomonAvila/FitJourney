a6b0e39edb56d52ae3392d8a7c5b05f93665f19e
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-08 12:09:18 -0500

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import '../styles/Home.css'
 import { client } from '../API/client'
 
 function Home(){
+
+    const [datosDeUsuario, establecerDatos] = useState(null)
+
     const obtenerDatos = async () => {
         const { data, error } = await client.auth.getUserIdentities()
+        if(error){
+            console.log("Error, revisa el codigo")
+        }else{
+            establecerDatos(data.user.id)
+        }
+    };
 
-    }
+    useEffect(() => {
+        obtenerDatos();
+    }, []);
     
 
     return (
         
         <div id="titulo">
-            <h1>Bienvenido {obtenerDatos}</h1>
+            <h1>Bienvenido, tu UID es: {uid ? uid : "Cargando..."}</h1>
         </div>
     )
 }
