2f8047d745f825bcff64bbb0701fa66f64a26fe7
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-08 15:37:42 -0500

    Prueba Cambio de informacion personal

--- src/pages/InformacionPerfil.jsx
+++ src/pages/InformacionPerfil.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { client } from "../API/client";
 import "../styles/InformacionPerfil.css";
+
 function InformacionPerfil() {
   const [perfil, setPerfil] = useState({
     id: "NULL",
@@ -8,6 +9,9 @@ function InformacionPerfil() {
     nombre: "NULL",
     fechaDeNacimiento: "NULL",
   });
+
+  const [nuevoNombre, setNombreNuevo] = useState(""); 
+  const [editandoNombre, setEditandoNombre] = useState(false);
 
   const extraerNombre = async () => {
     // Consulta en supabase
@@ -41,15 +45,52 @@ function InformacionPerfil() {
     obtenerNombre();
   }, []);
 
+  const actualizarNombre = async () => {
+    const {
+      data: { user },
+    } = await client.auth.getUser();
+
+    const { error } = await client
+      .from("usuario")
+      .update({ nombre: nuevoNombre })
+      .eq("id", user.id);
+
+    if (error) {
+      console.log("Error al actualizar nombre:", error);
+    } else {
+      const actualizado = await extraerNombre();
+      setPerfil(actualizado);
+      setNombreNuevo("");
+      setEditandoNombre(false);
+    }
+  };
+
+
+
   return (
     <div id="info">
       <h1>Informacion del perfil</h1>
-      <h2>ID: {perfil.id}</h2>
       <h2>Correo: {perfil.correo}</h2>
       <h2>Nombre: {perfil.nombre}</h2>
+      {!editandoNombre ? (
+        <button onClick={() => setEditandoNombre(true)}>Cambiar nombre</button>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Nuevo nombre"
+            value={nuevoNombre}
+            onChange={(e) => setNombreNuevo(e.target.value)}
+          />
+          <button onClick={actualizarNombre}>Guardar</button>
+          <button onClick={() => setEditandoNombre(false)}>Cancelar</button>
+        </div>
+      )}
       <h2>Fecha de nacimieto: {perfil.fechaDeNacimiento}</h2>
     </div>
   );
 }
+
+
 
 export default InformacionPerfil;
