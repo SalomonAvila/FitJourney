995f74c7af54a94bcae05270d772d561f6bee021
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-08 12:01:17 -0500

    Avance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import '../styles/Home.css'
-
+import { client } from '../API/client'
 
 function Home(){
+    const obtenerDatos = async () => {
+        const { data, error } = await client.auth.getUserIdentities()
+
+    }
+    
+
     return (
-        <div id="titulo">Home</div>
+        
+        <div id="titulo">
+            <h1>Bienvenido {obtenerDatos}</h1>
+        </div>
     )
 }
 export default Home
