5d7ac6009c48fd6a2f5da49cbd72a5d58f5dfa1a
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-08 13:45:44 -0500

    Estilos de prueba

--- src/pages/InformacionPerfil.jsx
+++ src/pages/InformacionPerfil.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import { client } from "../API/client";
-
+import '../styles/InformacionPerfil.css'
 function InformacionPerfil() {
   const [perfil, setPerfil] = useState({
     id: "NULL",
