78cc7b6ec7f1460c4f25e87e7bc6b2c0da006c37
Salomon Avila <104867468+SalomonAvila@users.noreply.github.com>
2025-05-09 22:05:45 +0000

    Envio base de datos

--- src/pages/AnadirRuta.jsx
+++ src/pages/AnadirRuta.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import {client} from "../API/client";
 function AnadirRuta() {
     const [rutas, setRutas] = useState([""]);
 
@@ -13,14 +13,38 @@ function AnadirRuta() {
         setRutas(nuevasRutas);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({e});
-    };
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+    const nombre = e.target.nombre.value;
 
+    try {
+        await actualizarBase(nombre, rutas);
+        console.log("Formulario enviado correctamente");
+    } catch (error) {
+        console.error("Error al enviar el formulario:", error);
+    }
+};
     const handleRetirarRuta = () => {
         if (rutas.length > 1) {
             setRutas(rutas.slice(0, -1));
+        }
+    }
+
+    const actualizarBase = async (nombre, rutas) => {
+        const {datas} = await client.auth.getUser();
+        const userId = datas.user.id;
+        const { data, error } = await client
+            .from("rutapersonalizada")
+            .insert({
+                    nombreruta: nombre,
+                    idusuario: userId,
+                    direcciones: rutas,
+                },
+            );
+        if (error) {
+            console.error("Error al insertar en la base de datos:", error);
+        } else {
+            console.log("Datos insertados correctamente:", data);
         }
     }
 
