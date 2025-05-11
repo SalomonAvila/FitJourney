31a54b600b0efab41cf118871d7376fd12a9ea5e
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-11 18:45:45 -0500

    Prueba modificacion barra de navegacion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./../styles/Navbar.css";
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+ 
   return (
     <nav>
       <h1>FitJourney</h1>
       <ul>
-        <li><a href="#home">Home</a></li>
+        <li><a href="#home" onClick={() => navigate("/")}>Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
