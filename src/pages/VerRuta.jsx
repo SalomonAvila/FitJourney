7ff86f0534161e889877e7a42244f360b8fd3e04
Salomon Avila <salomon.avila2506@gmail.com>
2025-05-11 19:12:50 -0500

    Prueba ver la ruta

--- src/pages/VerRuta.jsx
+++ src/pages/VerRuta.jsx
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { client } from "../API/client";
 
 function VerRuta(){
+    const [rutas, setRutas] = useState([])
+
+    const conseguirRutas = async () => {
+        try{
+            const {datos, error} = await client
+                                    .from("rutapersonalizada")
+                                    .select(`
+                                        idrutapersonalizada,
+                                        nombreruta,
+                                        direcciones,
+                                    `)
+                                    .eq("usuario.id", client.auth.getUser())
+            if(error){
+                console.log("Revisa el codigo")
+            }else{
+                setRutas(datos)
+            }
+        }catch(error){
+            console.log("Error, revisa el codigo")
+        }
+    }
+
+    useEffect(() => {
+        conseguirRutas()
+    }, [])
 
     return(
         <div>
-            <h1>
-                Prueba de visualizacion
-            </h1>
+            <h1>Prueba de visualización</h1>
+            <h2>Rutas asociadas al usuario:</h2>
+            <ul>
+                {rutas.map((ruta) => (
+                    <li key={ruta.idrutapersonalizada}>
+                        <strong>Nombre de la ruta:</strong> {ruta.nombreruta} <br />
+                        <strong>Direcciones:</strong> {ruta.direcciones.join(", ")} <br />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 
